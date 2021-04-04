3bd05501ae444524228110f49916aa9cb6fc0ee7
PabloSF <62628314+pabletefest@users.noreply.github.com>
2021-04-04 03:48:41 +0200

    Added sprites Player2 and ServiceLocator dessign pattern

---
+++ Assets/Scripts/Services/ServiceLocator.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine.Assertions;
+
+public class ServiceLocator
+{
+    private ServiceLocator instance;
+    public ServiceLocator Instance => instance ?? (instance = new ServiceLocator());
+
+    private Dictionary<Type, object> services;
+
+    private ServiceLocator()
+    {
+        services = new Dictionary<Type, object>();
+    }
+
+    public void RegisterService<T>(T service)
+    {
+        Type type = typeof(T);
+        Assert.IsFalse(services.ContainsKey(type), $"Service {type} already registered.");
+
+        services.Add(type, service);
+    }
+
+    public T GetService<T>()
+    {
+        Type type = typeof(T);
+
+        if(!services.TryGetValue(type, out object service))
+        {
+            throw new Exception($"Service {type} not found.");
+        }
+
+        return (T) service;
+    }
+
+    public void UnregisterService<T>()
+    {
+        Type type = typeof(T);
+
+        Assert.IsFalse(!services.ContainsKey(type), $"Service {type} can not be removed if it is not registered.");
+
+        services.Remove(type);
+    }
+
+    public void RemoveRegisteredServices()
+    {
+        services.Clear();
+    }
+}
