1c2c5998b73a0714979e5ff36d2f6492376c0d8f
PabloSF <62628314+pabletefest@users.noreply.github.com>
2021-04-05 18:22:21 +0200

    Character Controller, Joystick and Movement implemented

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
+
+	private Rigidbody2D m_Rigidbody2D;
+	
+	private bool isFacingRight = true; 
+	private Vector3 m_Velocity = Vector3.zero;
+
+
+	private void Awake()
+	{
+		m_Rigidbody2D = GetComponent<Rigidbody2D>();		
+	}
+
+    public void Move(float move)
+	{
+			
+			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+
+			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+			if (move > 0 && !isFacingRight)
+			{
+				Flip();
+			}
+			else if (move < 0 && isFacingRight)
+			{
+				Flip();
+			}
+			
+	}
+	
+
+
+	private void Flip()
+	{
+		isFacingRight = !isFacingRight;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+	public CharacterController controller;
+	public FloatingJoystick floatingJoystick;
+	Animator m_Animator;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	float verticalMove = 0f;
+
+    private void Start()
+    {
+		m_Animator = gameObject.GetComponent<Animator>();
+	}
+    // Update is called once per frame
+    void Update()
+	{
+		if (floatingJoystick.Horizontal >= .2f)
+		{
+			horizontalMove = runSpeed;
+		}
+		else if (floatingJoystick.Horizontal <= -.2f)
+		{
+			horizontalMove = -runSpeed;
+		}
+		else
+		{
+			horizontalMove = 0f;
+		}
+		m_Animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+		
+		//verticalMove = Input.GetAxisRaw("Vertical") * runSpeed;
+
+	}
+
+	void FixedUpdate()
+	{
+		controller.Move(horizontalMove * Time.fixedDeltaTime);
+	}
+}
