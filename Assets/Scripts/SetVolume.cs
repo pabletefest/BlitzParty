0911c20eeedb74707c862d3cb5ee8b8a3c2e5a9e
JOSE FRANCISCO GOMEZ ALEMANY <kikogomezalemany@gmail.com>
2021-05-16 17:36:13 +0200

    settingsVolume script and volumeMixer

---
+++ Assets/Scripts/SetVolume.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetAudioVolume(float sliderValue)
+    {
+        audioMixer.SetFloat("AudioVolume", Mathf.Log10(sliderValue) * 20);
+    }
+}
